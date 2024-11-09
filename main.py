02b13a4bbbfc29ea9a272abc7b7ea24a3fbc1db7
Joaqo <joaqo9909@gmail.com>
2024-11-09 16:43:26 -0300

    Acceso a propiedades de materiales y funcionalidad de agregar material

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import materiales
 # Ventana principal
 window = ttk.Window(themename="superhero")
 window.title("Transmission Loss Calculator")
-window.geometry("1300x700")
+window.geometry("1400x800")
 
 # Segmentación de contenedores
 window.rowconfigure(0, weight=1)  # Menús
 window.rowconfigure(1, weight=20) # Gráficos
 
 # Columnas de menús
-window.columnconfigure(0, minsize=600, weight=2)  
+window.columnconfigure(0, minsize=700, weight=2)  
 window.columnconfigure(1, minsize=400, weight=5)  
 window.columnconfigure(2, minsize=300, weight=2)  
 
@@ -51,17 +51,17 @@ for mat in materials_list:
 dimensiones_label = ttk.Label(frame_inferior, text="Dimensiones:", font="-weight bold")
 dimensiones_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-largo_label = ttk.Label(frame_inferior, text="Largo:")
+largo_label = ttk.Label(frame_inferior, text="Largo [m]:")
 largo_label.grid(row=0, column=1, padx=5, pady=5, sticky="e")
 largo_entry = ttk.Entry(frame_inferior, width=5)
 largo_entry.grid(row=0, column=2, padx=5, pady=5, sticky="w")
 
-alto_label = ttk.Label(frame_inferior, text="Alto:")
+alto_label = ttk.Label(frame_inferior, text="Alto [m]:")
 alto_label.grid(row=0, column=3, padx=5, pady=5, sticky="e")
 alto_entry = ttk.Entry(frame_inferior, width=5)
 alto_entry.grid(row=0, column=4, padx=5, pady=5, sticky="w")
 
-espesor_label = ttk.Label(frame_inferior, text="Espesor:")
+espesor_label = ttk.Label(frame_inferior, text="Espesor [mm]:")
 espesor_label.grid(row=0, column=5, padx=5, pady=5, sticky="e")
 espesor_entry = ttk.Entry(frame_inferior, width=5)
 espesor_entry.grid(row=0, column=6, padx=5, pady=5, sticky="w")
@@ -76,15 +76,48 @@ functions_frame.columnconfigure(1, weight=1)
 functions_frame.rowconfigure(0, weight=1)
 functions_frame.rowconfigure(1, weight=1)
 
-# Botones con ttkbootstrap
+# procesar
 boton_procesar = ttk.Button(functions_frame, text="Procesar", bootstyle=PRIMARY, padding=3, width=14)
-boton_agregar_material = ttk.Button(functions_frame, text="Agregar Material", bootstyle=SECONDARY, padding=3, width=14)
-boton_exportar = ttk.Button(functions_frame, text="Exportar", bootstyle=SECONDARY, padding=3, width=14)
-boton_borrar = ttk.Button(functions_frame, text="Borrar", bootstyle=DANGER, padding=3, width=14)
-
 boton_procesar.grid(row=0, column=0, padx=5, pady=5)
+
+
+#agregar material
+def agregar_material_command():
+    # Crear ventana emergente
+    add_mat_win = ttk.Toplevel(window)
+    add_mat_win.title("Agregar Material")
+    add_mat_win.geometry("300x200")
+    
+    # Etiquetas y campos de entrada para el nuevo material
+    nombre_label = ttk.Label(add_mat_win, text="Nombre del Material:")
+    nombre_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+    nombre_entry = ttk.Entry(add_mat_win, width=20)
+    nombre_entry.grid(row=0, column=1, padx=10, pady=10)
+    
+    densidad_label = ttk.Label(add_mat_win, text="Densidad [kg/m³]:")
+    densidad_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+    densidad_entry = ttk.Entry(add_mat_win, width=20)
+    densidad_entry.grid(row=1, column=1, padx=10, pady=10)
+    
+    espesor_label = ttk.Label(add_mat_win, text="Espesor [mm]:")
+    espesor_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+    espesor_entry = ttk.Entry(add_mat_win, width=20)
+    espesor_entry.grid(row=2, column=1, padx=10, pady=10)
+    
+    # Botón para confirmar la adición del material
+    confirmar_button = ttk.Button(add_mat_win, text="Agregar", bootstyle=SUCCESS)
+    confirmar_button.grid(row=3, column=0, columnspan=2, pady=10)
+    
+
+boton_agregar_material = ttk.Button(functions_frame, text="Agregar Material", bootstyle=SECONDARY, padding=3, width=14, command=agregar_material_command)
 boton_agregar_material.grid(row=0, column=1, padx=5, pady=5)
+
+#exportar
+boton_exportar = ttk.Button(functions_frame, text="Exportar", bootstyle=SECONDARY, padding=3, width=14)
 boton_exportar.grid(row=1, column=0, padx=5, pady=5)
+
+#borrar
+boton_borrar = ttk.Button(functions_frame, text="Borrar", bootstyle=DANGER, padding=3, width=14)
 boton_borrar.grid(row=1, column=1, padx=5, pady=5)
 
 ###########################
