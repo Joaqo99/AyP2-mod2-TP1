7333bc06d5abb3d072d64b5b08a7d2759ca7fa80
Joaqo <joaqo9909@gmail.com>
2024-11-11 09:12:43 -0300

    Correcciones de Sharp

--- acoustics_functions_Sharp.py
+++ acoustics_functions_Sharp.py
@@ -19,6 +19,117 @@ for i in range(0,len(f_to)-1):
     lon_onda.append(lon_onda_i)
 
 
+
+
+def sharp_method(material, l_y, l_x, espesor):
+    """
+    Calculates TL by Sharp Method. 
+    Input:
+        material: str type object. Material name.
+        espesor: float type object. Wall lenght.
+        l_y: float type object. Wall height.
+        l_x: float type object. Wall width.
+    """
+    t = espesor*1000    #paso el espesor de mm a m
+
+    material_props = materiales.get_material_properties(material)
+
+
+    ro_m = int(material_props["Densidad"])
+    e = float(material_props["Módulo de Young"])
+    n_in = float(material_props["Factor de pérdidas"])
+    poisson = float(material_props["Módulo Poisson"])
+
+
+    # Calculos ---------------------------------------------
+
+    #Masa superficial del material [kg/m2]
+    m_s = ro_m*t
+    #Superficie del elemento
+    s = l_x*l_y
+    #Rigidez
+    b = (e*(t**3))/(12*(1-(poisson**2)))
+    #Frecuencia crítica
+    f_c = ((c_o**2)/(2*np.pi))*((m_s/b)**(1/2))
+
+    #Frecuencia densidad
+    f_d = (e/(2*np.pi*ro_m))*((m_s/b)**(1/2))
+
+    r_sh_to = []
+
+
+    for i in range(len(f_to)):
+        if f_to[i] < (0.5*f_c):
+            r__0 = 10 * np.log10(1+((np.pi*m_s*f_to[i])/(ro_o*c_o))**2) - 5.5
+            r_sh_to.append(r__0)
+        elif f_to[i] >= f_c:
+            n_tot = n_in + (m_s/(485*(np.sqrt(f_to[i]))))
+            r__1 = 10 * np.log10(1+((np.pi*m_s*f_to[i])/(ro_o*c_o))**2) + 10 * np.log10((2*n_tot*f_to[i])/(np.pi*f_c))
+            r__2 = 10 * np.log10(1+((np.pi*m_s*f_to[i])/(ro_o*c_o))**2) - 5.5
+            r_out = min(r__1,r__2)
+            r_sh_to.append(r_out)
+        else:
+            r_sh_to.append(None)
+
+    #Encuentro las posiciones donde no hay valor y encuentro los maximos y minimos.
+
+    posiciones_none = [i for i, valor in enumerate(r_sh_to) if valor is None]
+
+    f_mid = []
+    r_mid = []
+
+    #busco el pico antes de la interpolación reemplanzando f = fc/2
+    valor_R_1 = 10 * np.log10(1+((np.pi*m_s*(f_c/2))/(ro_o*c_o))**2) - 5.5
+
+    #busco el último valor antes de q vuelva a sharp reemplazando f = fc
+    r___1 = 10 * np.log10(1+((np.pi*m_s*f_c)/(ro_o*c_o))**2) + 10 * np.log10((2*n_tot*f_c)/(np.pi*f_c))
+    r___2 = 10 * np.log10(1+((np.pi*m_s*f_c)/(ro_o*c_o))**2) - 5.5
+    valor_R_2 = min(r___1,r___2)
+
+    pendiente = (valor_R_2 - valor_R_1)/(f_c/2)
+    b = -(pendiente*f_c/2 - valor_R_1)
+
+
+    f_r_min_pos = min(posiciones_none)
+    f_r_max_pos = max(posiciones_none)
+    f_r_max_pos += 1
+    f_r_min_pos -= 1
+
+    f_r_min = f_to[f_r_min_pos]
+    f_r_max = f_to[f_r_max_pos]
+
+    # Armo listas para graficar interpolación entre puntos
+
+    f_mid.append(f_r_min)
+    f_mid.append(f_r_max)
+
+    r_mid.append(r_sh_to[f_r_min_pos])
+    r_mid.append(r_sh_to[f_r_max_pos])
+
+
+    R_sharp = []
+
+    #r_mid = np.linspace(r_sh_to[f_r_min_pos],r_sh_to[f_r_max_pos], f_r_max_pos - f_r_min_pos + 1, endpoint=True)
+    f_mid = np.linspace(f_r_min,f_r_max,f_r_max_pos - f_r_min_pos + 1,endpoint=True)
+    r_mid = f_mid*pendiente + b
+    r_mid = r_mid [1:-1]
+
+    R_sharp = []
+
+    R_mid_appended = 0
+    for i in r_sh_to:
+        if i != None:
+            R_sharp.append(i)
+        else:
+            R_sharp.append(r_mid[R_mid_appended])
+            R_mid_appended += 1
+
+
+    return R_sharp
+
+###############################################################
+# DESARROLLO DE PABLO
+
 # Datos de entrada -------------------------------------
 
 #ElementoPYL 
@@ -50,34 +161,10 @@ s = l_x*l_y
 b = (e*(t**3))/(12*(1-(poisson**2)))
 #Frecuencia crítica
 f_c = ((c_o**2)/(2*np.pi))*((m_s/b)**(1/2))
-   
-                                     
+                              
 #Frecuencia densidad
 f_d = (e/(2*np.pi*ro_m))*((m_s/b)**(1/2))
 
-
-
-#print(round(b,2),round(f_c,2),round(f_d,2))
-"""
-# Modelo Cremer ---------------------------
-
-
-
-r_to = []
-
-for i in range(len(f_to)):
-    if f_to[i]<f_c:
-        r = 20*np.log10(m_s*f_to[i]) - 47
-        r_to.append(r)
-    elif f_d>f_to[i]>f_c:
-        n_tot = n_in + (m_s/(485*(f_to[i]**(1/2))))
-        r_2 = 20*np.log10(m_s*f_to[i]) - 10*np.log10(np.pi/(4*n_tot)) + 10*np.log10(f_to[i]/f_c) - 10*np.log(f_c/(f_to[i]-f_c)) - 47
-        r_to.append(r_2)
-    else :
-        r_3 = 20*np.log10(m_s*f_to[i]) - 47
-        r_to.append(r_3)
-    
-"""    
 
 # Modelo Sharp ---------------------------
 
@@ -105,6 +192,19 @@ f_mid = []
 r_mid = []
 print(posiciones_none)
 
+
+#busco el pico antes de la interpolación reemplanzando f = fc/2
+valor_R_1 = 10 * np.log10(1+((np.pi*m_s*(f_c/2))/(ro_o*c_o))**2) - 5.5
+
+#busco el último valor antes de q vuelva a sharp reemplazando f = fc
+r___1 = 10 * np.log10(1+((np.pi*m_s*f_c)/(ro_o*c_o))**2) + 10 * np.log10((2*n_tot*f_c)/(np.pi*f_c))
+r___2 = 10 * np.log10(1+((np.pi*m_s*f_c)/(ro_o*c_o))**2) - 5.5
+valor_R_2 = min(r___1,r___2)
+
+pendiente = (valor_R_2 - valor_R_1)/(f_c/2)
+b = -(pendiente*f_c/2 - valor_R_1)
+
+
 f_r_min_pos = min(posiciones_none)
 f_r_max_pos = max(posiciones_none)
 f_r_max_pos += 1
@@ -126,14 +226,25 @@ r_mid.append(r_sh_to[f_r_max_pos])
 
 R_sharp = []
 
-r_mid = np.linspace(r_sh_to[f_r_min_pos],r_sh_to[f_r_max_pos], f_r_max_pos - f_r_min_pos + 1, endpoint=True)
+#r_mid = np.linspace(r_sh_to[f_r_min_pos],r_sh_to[f_r_max_pos], f_r_max_pos - f_r_min_pos + 1, endpoint=True)
+f_mid = np.linspace(f_r_min,f_r_max,f_r_max_pos - f_r_min_pos + 1,endpoint=True)
+r_mid = f_mid*pendiente + b
 r_mid = r_mid [1:-1]
-#f_mid = np.linspace(f_r_min,f_r_max,f_r_max_pos - f_r_min_pos,endpoint=True)
 
+pendiente_mansilla = (33.11-36.29)/(f_c/2)
+b_mansilla = -pendiente_mansilla*1600 + 36.29
 
+print("")
+print(f"Pendiente: {pendiente}")
+print(f"b: {b}")
+
+print(f"Pendiente mansilla: {pendiente_mansilla}")
+print(f"B mansilla: {b_mansilla}")
+print("")
 print(f"R mid {r_mid}")
 
 print(f"F mid {f_mid}")
+print("")
 
 
 
@@ -149,7 +260,9 @@ for i in r_sh_to:
 
 print(f"NEW Sharp {R_sharp}")
 
+R_sharp_2 = sharp_method("PYL", l_y, l_x, 12.5)
 
+print(f"Sharp function: {R_sharp_2}")
 
 
 
@@ -192,88 +305,3 @@ plt.tight_layout()
 plt.show()
 
 
-
-
-
-
-
-
-
-
-
-################################
-
-
-
-
-def sharp_method(material, l_y, l_x, espesor):
-    """
-    Calculates TL by Sharp Method. 
-    Input:
-        material: str type object. Material name.
-        espesor: float type object. Wall lenght.
-        l_y: float type object. Wall height.
-        l_x: float type object. Wall width.
-    """
-    t = espesor*1000    #paso el espesor de mm a m
-
-    material_props = materiales.get_material_properties(material)
-
-
-    ro_m = material_props["Densidad"]
-    e = material_props["Módulo de Young"]
-    n_in = material_props["Factor de pérdidas"]
-    poisson = material_props["Módulo Poisson"]
-
-
-    r_sh_to = []
-
-
-    for i in range(len(f_to)):
-        if f_to[i] < (0.5*f_c):
-            r__0 = 10 * np.log10(1+((np.pi*m_s*f_to[i])/(ro_o*c_o))**2) - 5.5
-            r_sh_to.append(r__0)
-        elif f_to[i] >= f_c:
-            n_tot = n_in + (m_s/(485*(np.sqrt(f_to[i]))))
-            r__1 = 10 * np.log10(1+((np.pi*m_s*f_to[i])/(ro_o*c_o))**2) + 10 * np.log10((2*n_tot*f_to[i])/(np.pi*f_c))
-            r__2 = 10 * np.log10(1+((np.pi*m_s*f_to[i])/(ro_o*c_o))**2) - 5.5
-            r_out = min(r__1,r__2)
-            r_sh_to.append(r_out)
-        else:
-            r_sh_to.append(None)
-
-    #Encuentro las posiciones donde no hay valor y encuentro los maximos y minimos.
-
-    posiciones_none = [i for i, valor in enumerate(r_sh_to) if valor is None]
-
-    f_mid = []
-    r_mid = []
-    print(posiciones_none)
-    f_r_min_pos = min(posiciones_none)
-    f_r_max_pos = max(posiciones_none)
-    f_r_max_pos += 1
-    f_r_min_pos -= 1
-
-    f_r_min = f_to[f_r_min_pos]
-    f_r_max = f_to[f_r_max_pos]
-
-    # Armo listas para graficar interpolación entre puntos
-
-    f_mid.append(f_r_min)
-    f_mid.append(f_r_max)
-
-    r_mid.append(r_sh_to[f_r_min_pos])
-    r_mid.append(r_sh_to[f_r_max_pos])
-
-    R_sharp = np.array([])
-
-    R_mid_appended = 0
-    for i in r_sh_to:
-        if i:
-            R_sharp.append(i)
-        else:
-            R_sharp.append(r_mid[R_mid_appended])
-            R_mid_appended += 1
-
-
-    return R_sharp
