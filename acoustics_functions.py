bd0824263407f209b0b5de928fa0cc197d2df12f
LucaFalchini <lucafalchini@gmail.com>
2024-11-11 23:09:45 -0300

    Formula ISO12354

--- acoustics_functions.py
+++ acoustics_functions.py
@@ -49,57 +49,86 @@ f_c = ((c_o**2)/(2*np.pi))*((m_s/b)**(1/2))
 #Frecuencia densidad
 f_d = (e/(2*np.pi*ro_m))*((m_s/b)**(1/2))
 
+#Modelo de Cremer - Ley de masa--------------------------------------
+def mod_cremer_fun(f_to, f_c, f_d, m_s):
+    '''
+    Cremer model
+    '''
+    r_to = []
 
+    for i in range(len(f_to)):
+        if f_to[i]<f_c:
+            r = 20*np.log10(m_s*f_to[i]) - 47
+            r_to.append(r)
+        elif f_d>f_to[i]>f_c:
+            n_tot = n_in + (m_s/(485*(f_to[i]**(1/2))))
+            r_2 = 20*np.log10(m_s*f_to[i]) - 10*np.log10(np.pi/(4*n_tot)) + 10*np.log10(f_to[i]/f_c) - 10*np.log(f_c/(f_to[i]-f_c)) - 47
+            r_to.append(r_2)
+        else :
+            r_3 = 20*np.log10(m_s*f_to[i]) - 47
+            r_to.append(r_3)
+    return r_to
+    
+    
+# Modelo ISO 12354 -----------------------------------------------------
 
-#print(round(b,2),round(f_c,2),round(f_d,2))
+#Factor de radiación para ondas de flexión libres
+sigma = [] #El que calculo Pablo
 
-
-r_to = []
-
+#Numero de onda
+k_o = []
 for i in range(len(f_to)):
-    if f_to[i]<f_c:
-        r = 20*np.log10(m_s*f_to[i]) - 47
-        r_to.append(r)
-    elif f_d>f_to[i]>f_c:
-        n_tot = n_in + (m_s/(485*(f_to[i]**(1/2))))
-        r_2 = 20*np.log10(m_s*f_to[i]) - 10*np.log10(np.pi/(4*n_tot)) + 10*np.log10(f_to[i]/f_c) - 10*np.log(f_c/(f_to[i]-f_c)) - 47
-        r_to.append(r_2)
-    else :
-        r_3 = 20*np.log10(m_s*f_to[i]) - 47
-        r_to.append(r_3)
-    
-    
+    k_o_i = 2*np.pi*f_to[i]
+    k_o.append(k_o_i)
 
-print(f_to[i],round(f_c,2),round(f_d,2),n_in, n_tot, r_2, r_to)
+def nabla_fun(k_o, l_x, l_y):
+    '''
+    nabla
+    '''
+    nabla = []
+    for i in range(len(f_to)):
+        nabla_i = -0.964 - (0.5+(l_y/(np.pi*l_x)))*np.log(l_y/l_x) + ((5*l_y)/(2*np.pi*l_x)) - (1/(4*np.pi*l_x*l_y*(k_o[i]**2)))
+        nabla.append(nabla_i)
+    return nabla
 
-# Datos de ejemplo
-frecuencias = f_to  # Frecuencias de bandas de octava en Hz
-reduccion_sonora = r_to  # Valores de reducción sonora en dB
+def sigma_f_fun(nabla, k_o, l_x, l_y):
+    '''
+    forced wave radiation factor
+    '''
+    sigma_f =[]
+    for i in range(len(f_to)):
+        if sigma[i]<=2:
+            sigma_f_i = 0.5*(np.log(k_o[i]*((l_x*l_y)**(1/2)))-nabla[i])
+        else:
+            sigma_f_i = 2
+        sigma_f.append(sigma_f_i)    
+    return sigma_f
 
-# Estilo moderno
-sns.set_theme(style="whitegrid")
-plt.figure(figsize=(10, 6))
-
-# Gráfico de líneas
-plt.plot(frecuencias, reduccion_sonora, marker='o', color='b', linestyle='-', linewidth=2, markersize=6, label='Reducción Sonora')
-
-# Etiquetas y título
-plt.xlabel("Frecuencia (Hz)", fontsize=14)
-plt.ylabel("Reducción Sonora (dB)", fontsize=14)
-plt.title("Reducción Sonora por Bandas de Octava", fontsize=16)
-plt.yscale("linear")  # Cambia a 'log' si necesitas una escala logarítmica
-plt.xscale("log")  # Escala logarítmica en el eje x
-
-bandas_octava = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
-plt.xticks(bandas_octava, labels=bandas_octava, rotation=45)
-
-plt.legend()
-plt.grid(True, linestyle='--', alpha=0.7)
-plt.tight_layout()
-
-# Mostrar gráfico
-plt.show()
-
-print("Luca")
-print("Apellido")
-print("Prueba de Branch")
+def r_to_iso_fun(sigma, sigma_f, l_x, l_y, f_to):
+    '''
+    Calculates the R by ISO12354
+    sigma: array. free bending waves radiation factor
+    sigma_f: array. forced bending waves radiation factor
+    n_tot: array. total loss factor
+    l_x: float. width
+    l_y: float. height
+    f_to: array. frecuency per third octave band
+    '''
+    r_to_iso = []
+    for i in range(len(f_to)):
+        #Factor de transmisión por banda de tercio de octavas
+        tau_a = ((2*ro_o*c_o)/(2*np.pi*f_to[i]*m_s))**2
+        tau_b = ((sigma[i])**2)/(n_in + (m_s/(485*((f_to[i])**(1/2)))))
+        if f_to[i]<f_c:
+            tau = tau_a*(tau_b*(np.pi*f_c/(2*f_to[i])))
+            r_i = -10*np.log10(tau)
+            r_to_iso.append(r_i)
+        elif f_to[i]>f_c:
+            tau = tau_a*(2*sigma_f + ((l_x+l_y)**2)/(l_x**2+l_y**2)*((f_c/f_to[i])**(1/2))*tau_b)
+            r_i = -10*np.log10(tau)
+            r_to_iso.append(r_i)
+        else:
+            tau = tau_a*(tau_b*np.pi/2)
+            r_i = -10*np.log10(tau)
+            r_to_iso.append(r_i)
+    return r_to_iso
